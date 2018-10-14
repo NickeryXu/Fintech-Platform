de7c1b49980e5fed7060ef804e76b375b80097e7
xuyilin <xuyilin@orientsoft.cn>
2018-10-14 17:45:15 +0800

    动态布局

--- fintech-server/applications/courses.py
+++ fintech-server/applications/courses.py
@@ -29,69 +29,69 @@ def course_series():
     finally:
         return render_template('course.html', courses=returnObj['data']['courses'])
 
-#二级目录
-@sign_check()
-@course.route('/course/<courseId>', methods=['GET'])
-def course_list(courseId):
-    from models.course import COURSE
-    returnObj = {}
-    try:
-        courseId = ObjectId(courseId)
-        courseObj = []
-        data_course = COURSE.objects(id=courseId).first()
-        for data in data_course.detail.courses:
-            dataObj = {}
-            dataObj['uid'] = data['uid']
-            dataObj['course'] = data['course']
-            courseObj.append(dataObj)
-        returnObj['data'] = {'courses': courseObj}
-        returnObj['info'] = {'result': 1, 'info': '获取成功'}
-    except Exception as e:
-        print('二级目录:', e)
-        returnObj['data'] = {}
-        returnObj['info'] = {'result': 500, 'info': '后台异常'}
-    finally:
-        return render_template('learn.html', courses=returnObj['data']['courses'])
-
-#课程内容
-@sign_check()
-@course.route('/course/<courseId>/<uid>', methods=['GET'])
-def course_detail(courseId, uid):
-    from models.course import COURSE
-    from models.user import USER
-    returnObj = {}
-    try:
-        dataObj = {}
-        courseId = ObjectId(courseId)
-        data = COURSE.objects(id=courseId).first()
-        courses = data.detail.courses
-        for course in courses:
-            if course['uid'] == uid:
-                dataObj['uid'] = uid
-                dataObj['course'] = course['course']
-                dataObj['description'] = course['description']
-                dataObj['remark'] = course['remark']
-                dataObj['datalink'] = course['datalink']
-                userObj = {'course': dataObj['course'], 'id': courseId, 'uid': uid}
-                data_user = USER.objects(id=ObjectId(session['id'])).first()
-                data_courses = data_user.courses
-                i = 0
-                for n in data_courses:
-                    if n['id'] == courseId:
-                        n['uid'] = uid
-                        n['course'] = course['course']
-                        data_user.save()
-                        i = 1
-                if i != 1:
-                    USER.objects(id=ObjectId(session['id'])).update_one(push__courses=userObj)
-        returnObj['data'] = dataObj
-        returnObj['info'] = {'result': 1, 'info': '获取成功'}
-    except Exception as e:
-        print('课程内容:', e)
-        returnObj['data'] = {}
-        returnObj['info'] = {'result': 500, 'info': '后台异常'}
-    finally:
-        return jsonify(returnObj)
+# #二级目录
+# @sign_check()
+# @course.route('/course/<courseId>', methods=['GET'])
+# def course_list(courseId):
+#     from models.course import COURSE
+#     returnObj = {}
+#     try:
+#         courseId = ObjectId(courseId)
+#         courseObj = []
+#         data_course = COURSE.objects(id=courseId).first()
+#         for data in data_course.detail.courses:
+#             dataObj = {}
+#             dataObj['uid'] = data['uid']
+#             dataObj['course'] = data['course']
+#             courseObj.append(dataObj)
+#         returnObj['data'] = {'courses': courseObj}
+#         returnObj['info'] = {'result': 1, 'info': '获取成功'}
+#     except Exception as e:
+#         print('二级目录:', e)
+#         returnObj['data'] = {}
+#         returnObj['info'] = {'result': 500, 'info': '后台异常'}
+#     finally:
+#         return render_template('learn.html', courses=returnObj['data']['courses'])
+#
+# #课程内容
+# @sign_check()
+# @course.route('/course/<courseId>/<uid>', methods=['GET'])
+# def course_detail(courseId, uid):
+#     from models.course import COURSE
+#     from models.user import USER
+#     returnObj = {}
+#     try:
+#         dataObj = {}
+#         courseId = ObjectId(courseId)
+#         data = COURSE.objects(id=courseId).first()
+#         courses = data.detail.courses
+#         for course in courses:
+#             if course['uid'] == uid:
+#                 dataObj['uid'] = uid
+#                 dataObj['course'] = course['course']
+#                 dataObj['description'] = course['description']
+#                 dataObj['remark'] = course['remark']
+#                 dataObj['datalink'] = course['datalink']
+#                 userObj = {'course': dataObj['course'], 'id': courseId, 'uid': uid}
+#                 data_user = USER.objects(id=ObjectId(session['id'])).first()
+#                 data_courses = data_user.courses
+#                 i = 0
+#                 for n in data_courses:
+#                     if n['id'] == courseId:
+#                         n['uid'] = uid
+#                         n['course'] = course['course']
+#                         data_user.save()
+#                         i = 1
+#                 if i != 1:
+#                     USER.objects(id=ObjectId(session['id'])).update_one(push__courses=userObj)
+#         returnObj['data'] = dataObj
+#         returnObj['info'] = {'result': 1, 'info': '获取成功'}
+#     except Exception as e:
+#         print('课程内容:', e)
+#         returnObj['data'] = {}
+#         returnObj['info'] = {'result': 500, 'info': '后台异常'}
+#     finally:
+#         return jsonify(returnObj)
 
 #我的课程
 @sign_check()
@@ -146,3 +146,61 @@ def homework_put(courseId, uid):
         returnObj['info'] = {'result': 500, 'info': '后台异常'}
     finally:
         return jsonify(returnObj)
+
+#课程内容
+@sign_check()
+@course.route('/course/<courseId>', methods=['GET'])
+def course_detail(courseId):
+    from models.course import COURSE
+    from models.user import USER
+    returnObj = {}
+    try:
+        courseId = ObjectId(courseId)
+        data_course = COURSE.objects(id=courseId).first()
+        courseArray = []
+        for data in data_course.detail.courses:
+            dataObj = {}
+            dataObj['uid'] = data['uid']
+            dataObj['course'] = data['course']
+            courseArray.append(dataObj)
+        data_user = USER.objects(id=session['id']).first()
+        n = 0
+        courses = data_user.courses
+        for course in courses:
+            if course['id'] == courseId:
+                n = course['uid']
+        if n == 0:
+            uid = 1
+        else:
+            uid = n
+        courses_detail = data_course.detail.courses
+        courseObj = {}
+        for course in courses_detail:
+            if course['uid'] == uid:
+                courseObj['uid'] = uid
+                courseObj['course'] = course['course']
+                courseObj['description'] = course['description']
+                courseObj['remark'] = course['remark']
+                courseObj['datalink'] = course['datalink']
+                courseObj['env'] = course['env']
+                userObj = {'course': courseObj['course'], 'id': courseId, 'uid': uid}
+                data_user = USER.objects(id=ObjectId(session['id'])).first()
+                data_courses = data_user.courses
+                i = 0
+                for n in data_courses:
+                    if n['id'] == courseId:
+                        n['uid'] = uid
+                        n['course'] = course['course']
+                        data_user.save()
+                        i = 1
+                if i != 1:
+                    USER.objects(id=ObjectId(session['id'])).update_one(push__courses=userObj)
+        returnObj['courses'] = courseArray
+        returnObj['course'] = courseObj
+    except Exception as e:
+        print('课程内容:', e)
+        returnObj['data'] = {}
+        returnObj['info'] = {'result': 500, 'info': '后台异常'}
+    finally:
+        return render_template('learn.html', courses=returnObj['courses'], course=returnObj['course'])
+
